145b877c3b96c63f3aee30b740070b72c9132883
Ruben <Rthem@gmx.de>
2021-12-11 16:05:44 +0100

    sorting traefik

--- portainer(standalone)/shell.sh
+++ portainer(standalone)/shell.sh
@@ -1,4 +1,2 @@
 docker volume create portainer_data
-docker run -d -p 9000:9000 --name pt --restart unless-stopped -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
-
-
+docker run -d -p 9000:9000 -p 8000:8000 --name pt --restart unless-stopped -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
